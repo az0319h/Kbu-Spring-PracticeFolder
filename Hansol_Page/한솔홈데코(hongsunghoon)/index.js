72712dfb5b904b5ec21cb850a8a2f2ea3206a12a
digital-shield-hoon <49442904+az0319h@users.noreply.github.com>
2024-03-28 23:33:50 +0900

    한솔홈데코 헤더 및 비주얼 영역 클론 코딩 결과 제출

---
+++ Hansol_Page/한솔홈데코(hongsunghoon)/index.js
@@ -0,0 +1,4 @@
+document.querySelector('#slick-slide-control00').innerHTML = "인테리어자제"
+document.querySelector('#slick-slide-control01').innerHTML = "경화마루"
+document.querySelector('#slick-slide-control02').innerHTML = "SB마루"
+document.querySelector('#slick-slide-control03').innerHTML = "경마루"
